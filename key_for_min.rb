c426b8c645291fb07ac2c98eb7356f9415ac78dd
Ian Pollack <ianjpollack@gmail.com>
2018-03-27 18:00:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(name_hash)
     return nil
   else
     firstValue = 0
-    binding.pry
+
     name_hash.each do |key, value|
       if value < firstValue
         firstValue = value
