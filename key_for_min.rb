3429825db79db4dca1019ae597bb0108d6e7df0d
Ian Pollack <ianjpollack@gmail.com>
2018-03-27 18:33:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
+    minKey = 0
     testerValue = name_hash.first[1]
     name_hash.each do |key, value|
       if value < testerValue
